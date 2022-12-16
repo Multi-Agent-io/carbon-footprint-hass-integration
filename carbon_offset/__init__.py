fbc24f807534c60923079e8785b54b715f4207e0
arseniiarsenii <arseniivelichko2@gmail.com>
2022-12-16 22:54:14 +0200

    fix: Added missing __init__.py

---
+++ carbon_offset/__init__.py
@@ -0,0 +1,9 @@
+from .carbon_asset_burner import burn_carbon_asset, burn_carbon_asset_async
+from .carbon_asset_calculator import calculate_compensation_amt_tokens, calculate_compensation_amt_tokens_async
+
+__all__ = (
+    "calculate_compensation_amt_tokens_async",
+    "calculate_compensation_amt_tokens",
+    "burn_carbon_asset_async",
+    "burn_carbon_asset",
+)
